aa2df0a4496abcb29879abd6a27fb5c14172a84b
Mike Pry <mikepry@hotmail.com>
2020-07-25 18:53:55 -0400

    Modified App file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,29 @@ class App extends Component {
     };
   }
 
+  updateInput(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice(),
+    };
+    const list = [...this.state.list];
+    list.push(newItem);
+    this.setState({
+      list,
+      newItem: "",
+    });
+  }
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter((item) => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
@@ -22,7 +45,18 @@ class App extends Component {
             value={this.state.newItem}
             onChange={(e) => this.updateInput("newItem", e.target.value)}
           />
-          <button></button>
+          <button onClick={() => this.addItem()}>Add</button>
+          <br />
+          <ul>
+            {this.state.list.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>x</button>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     );
