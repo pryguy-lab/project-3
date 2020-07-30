36a90949718b9fe2e0eefb6a378e1e49d83ce317
Mike Pry <mikepry@hotmail.com>
2020-07-29 22:48:13 -0400

    added some styling

--- src/App.js
+++ src/App.js
@@ -35,36 +35,47 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello Ella!</h1>
-        <div>
-          Add an item...
-          <br />
-          <input
-            type="text"
-            placeholder="Type item here..."
-            value={this.state.newItem}
-            onChange={(e) => this.updateInput("newItem", e.target.value)}
-          />
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            onClick={() => this.addItem()}
-          >
-            Add
-          </button>
-          <br />
-          <ul>
-            {this.state.list.map((item) => {
-              return (
-                <li key={item.id}>
-                  {item.value}
-                  <button onClick={() => this.deleteItem(item.id)}>
-                    Delete
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <body class="border-8 border-gray-600 text-center h-screen bg-blue-100">
+          <header class="text-6xl text-blue-400">todo list</header>
+          <div class="text-4xl mb-8 ml-4">
+            Add an item...
+            <br />
+          </div>
+          <div>
+            <input
+              class="bg-blue-200 hover:bg-blue-400 text-black font-bold py-2 px-4 mx-4 placeholder-purple-900 mb-4"
+              type="text"
+              placeholder="Type item here..."
+              value={this.state.newItem}
+              onChange={(e) => this.updateInput("newItem", e.target.value)}
+            />
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => this.addItem()}
+            >
+              Add
+            </button>
+            <br />
+            <ul>
+              {this.state.list.map((item) => {
+                return (
+                  <li
+                    class="text-black font-bold text-1xl text-center"
+                    key={item.id}
+                  >
+                    {item.value}
+                    <button
+                      class="ml-8 bg-red-700 hover:bg-red-500 text-white font-bold mt-3 py-1 px-1 rounded"
+                      onClick={() => this.deleteItem(item.id)}
+                    >
+                      Remove
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </body>
       </div>
     );
   }
